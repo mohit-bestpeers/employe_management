d2ef0c5360570b520279660e1807f445d31b0c98
mohit-bestpeers <mobodkhe@bestpeers.com>
2023-07-06 15:04:02 +0530

    Added show action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,4 +4,10 @@ class EmployeesController < ApplicationController
     @employee = Employee.all
     render json: @employee
   end
+
+  def show
+    @employee = Employee.find(params[:id])
+    render json: @employee
+  end
+
 end
