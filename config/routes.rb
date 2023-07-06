164619cca1d7a67b4bd183a27f273815e5bf0e15
mohit-bestpeers <mobodkhe@bestpeers.com>
2023-07-06 17:02:15 +0530

    Added create and destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :employees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   resources :employees
-
+ 
 end
